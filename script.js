3b2c5d3b0fd418a3f558fa7206de1e1999595421
kennethbchen <kennethbchen@gmail.com>
2023-05-11 21:21:40 -0400

    removed print

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const countSpan = document.getElementById("loop-count")
 const startTime = Date.now()
 
 setInterval( _ => {
-    // Seconds
-    let elapsed = (Date.now() - startTime) / 1000;
-    console.log(elapsed);
+    
+    let elapsed = (Date.now() - startTime) / 1000; // Seconds
     countSpan.innerHTML = Math.floor(elapsed);
 
 }, 1000)
